1c4c973fe4274f792dcf637b00fc6411aa5d6e1e
unknown <tarantylhomie@gmail.com>
2023-04-01 18:47:16 +0300

    completed Task_47

---
+++ Task_47/Program.cs
@@ -0,0 +1,12 @@
+﻿double[,] a = new double[3, 4];
+
+Random random = new Random();
+for (int m = 0; m < 5; m++)
+{
+    for (int n = 0; n < 4; n++)
+    {
+        a[m, n] = random.NextDouble() * 10; // NextDouble() дает случайное вещественное число в диапазоне от 0 до 1
+        Console.Write("{0,6:F2}", a[m, n]);
+    }
+    Console.WriteLine();
+}
